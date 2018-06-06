abf39ec811013328e44482a7a84f650f6cf1aa6a
KenwoodFox <31870999+KenwoodFox@users.noreply.github.com>
2018-06-06 16:44:14 -0400

    Updated the things

--- src/org/usfirst/frc/team1721/robot/Robot.java
+++ src/org/usfirst/frc/team1721/robot/Robot.java
@@ -16,13 +16,7 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import org.usfirst.frc.team1721.robot.commands.*;
 import org.usfirst.frc.team1721.robot.subsystems.*;
 
-/**
- * The VM is configured to automatically run this class, and to call the
- * functions corresponding to each mode, as described in the TimedRobot
- * documentation. If you change the name of this class or the package after
- * creating this project, you must also update the build.properties file in the
- * project.
- */
+import com.ctre.phoenix.motorcontrol.can.VictorSPX;
 
 public class Robot extends TimedRobot {
 	public static final Autonomous kExampleSubsystem
@@ -30,28 +24,22 @@ public class Robot extends TimedRobot {
 	public static OI m_oi;
 
 	Command m_autonomousCommand;
-	SendableChooser<Command> m_chooser = new SendableChooser<>();
+	SendableChooser<Command> autoChooser = new SendableChooser<>();
 
-	/**
-	 * This function is run when the robot is first started up and should be
-	 * used for any initialization code.
-	 */
 	@Override
 	public void robotInit() {
-		m_oi = new OI();
-		m_chooser.addDefault("Default Auto", new ExampleCommand());
+		RobotMap.motorPort = new VictorSPX(RobotMap.portAddress); //Define location of port motor
+		RobotMap.motorStarboard = new VictorSPX(RobotMap.starboardAddress); //Define Location of starboard motor
+		
+		autoChooser.addDefault("Default Auto", new ExampleCommand());
 		// chooser.addObject("My Auto", new MyAutoCommand());
-		SmartDashboard.putData("Auto mode", m_chooser);
+		SmartDashboard.putData("Auto mode", autoChooser);
 	}
 
-	/**
-	 * This function is called once each time the robot enters Disabled mode.
-	 * You can use it to reset any subsystem information you want to clear when
-	 * the robot is disabled.
-	 */
+	
 	@Override
 	public void disabledInit() {
-
+		//reset things here
 	}
 
 	@Override
@@ -59,28 +47,10 @@ public class Robot extends TimedRobot {
 		Scheduler.getInstance().run();
 	}
 
-	/**
-	 * This autonomous (along with the chooser code above) shows how to select
-	 * between different autonomous modes using the dashboard. The sendable
-	 * chooser code works with the Java SmartDashboard. If you prefer the
-	 * LabVIEW Dashboard, remove all of the chooser code and uncomment the
-	 * getString code to get the auto name from the text box below the Gyro
-	 *
-	 * <p>You can add additional auto modes by adding additional commands to the
-	 * chooser code above (like the commented example) or additional comparisons
-	 * to the switch structure below with additional strings & commands.
-	 */
 	@Override
 	public void autonomousInit() {
-		m_autonomousCommand = m_chooser.getSelected();
-
-		/*
-		 * String autoSelected = SmartDashboard.getString("Auto Selector",
-		 * "Default"); switch(autoSelected) { case "My Auto": autonomousCommand
-		 * = new MyAutoCommand(); break; case "Default Auto": default:
-		 * autonomousCommand = new ExampleCommand(); break; }
-		 */
-
+		m_autonomousCommand = autoChooser.getSelected();
+		
 		// schedule the autonomous command (example)
 		if (m_autonomousCommand != null) {
 			m_autonomousCommand.start();
--- src/org/usfirst/frc/team1721/robot/RobotMap.java
+++ src/org/usfirst/frc/team1721/robot/RobotMap.java
@@ -7,20 +7,11 @@
 
 package org.usfirst.frc.team1721.robot;
 
-/**
- * The RobotMap is a mapping from the ports sensors and actuators are wired into
- * to a variable name. This provides flexibility changing wiring, makes checking
- * the wiring easier and significantly reduces the number of magic numbers
- * floating around.
- */
-public class RobotMap {
-	// For example to map the left and right motors, you could define the
-	// following variables to use with your drivetrain subsystem.
-	// public static int leftMotor = 1;
-	// public static int rightMotor = 2;
+import com.ctre.phoenix.motorcontrol.can.VictorSPX; //import to use the VictorSPX
 
-	// If you are using multiple modules, make sure to define both the port
-	// number and the module. For example you with a rangefinder:
-	// public static int rangefinderPort = 1;
-	// public static int rangefinderModule = 1;
+public class RobotMap { //Robotmap main class
+	public static int portAddress = 0; //Port CAN address
+	public static int starboardAddress = 1; //Starboard CAN address
+	public static VictorSPX motorPort, motorStarboard; //Drive motors
+	public static VictorSPX utilityMotorOne; //Function motors
 }
